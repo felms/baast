1637e8a06611c753f04c7b499ed57b928661f842
Felipe <felipemoreira1@gmail.com>
2022-04-03 16:41:53 -0300

    Adcionei o BD e atualizei as rotas

--- index.js
+++ index.js
@@ -1,37 +1,102 @@
 const express = require("express");
+const mysql = require("mysql");
+
 const server = express();
 server.use(express.json());
 
-const geeks = [];
-
-server.get("/geeks", (req, res) => {
-    return res.json(geeks);
+//Cria a conexão
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'S#nha01Teste',
+    database : 'pets'
 });
 
-server.get("/geeks/:index", (req, res) => {
-    return res.json(geeks[req.params.index]);
+// Conecta
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("MySQL conectado...")
+    }
+});
+
+
+// ---- Rotas ----
+
+// Get
+server.get("/cats", (req, res) => {
+    db.query("SELECT * FROM pets.cat", (err, result) => {
+        if (err) {
+            console.log(err)
+        } 
+        
+        res.send(result);
+    });
+    
+});
+
+// Get com id
+server.get("/cats/:index", (req, res) => {
+    const {index} = req.params;
+    db.query("SELECT * FROM pets.cat WHERE id = ?", index, (err, result) => {
+        if (err) {
+            console.log(err)
+        } 
+        
+        res.send(result);
+    });
 })
 
-server.post("/geeks", (req, res) => {
-    const {name} = req.body;
-    geeks.push(name);
-    return res.json(geeks);
+// Post
+server.post("/cats", (req, res) => {
+    
+    let sql = "INSERT INTO pets.cat (name, owner, birth) VALUES (?, ?, ?)";
+    db.query(sql, [req.body.name, req.body.owner, req.body.birth], (err, result) => {
+        if (err) {
+            console.log(err)
+        } 
+        
+        res.send(result);
+    });
+
 });
 
-server.put("/geeks/:index", (req, res) => {
+// Update/Put
+server.put("/cats/:index", (req, res) => {
     const {index} = req.params;
-    const {name} = req.body;
 
-    geeks[index] = name;
+    let sql = "UPDATE pets.cat SET name = ?, owner = ?, birth = ? WHERE id = ?";
 
-    return res.json(geeks);
+    db.query(sql, [req.body.name, req.body.owner, req.body.birth, index], (err, result) => {
+        if (err) {
+            console.log(err)
+        } 
+        
+        res.send(result);
+    });
+
 });
 
-server.delete("/geeks/:index", (req, res) => {
+
+// Delete
+server.delete("/cats/:index", (req, res) => {
     const {index} = req.params;
-    geeks.splice(index, 1);
 
-    return res.send();
+    let sql = "DELETE FROM pets.cat WHERE id = ?";
+
+    db.query(sql, index, (err, result) => {
+        if (err) {
+            console.log(err)
+        } 
+        
+        res.send(result);
+    });
 });
 
-server.listen(3000);
+
+// ------
+
+server.listen(3000, () => {
+    console.log("Servidor em execução na porta: 3000");
+});
